6a923ed8fd7c78cb1d643d1e1f3a945089a2b642
Phillip Adkins <padkins@pca.local>
2016-06-18 13:19:42 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'fcollections',
+  packages = ['fcollections'], # this must be the same as the name above
+  version = '0.1',
+  description = 'collections with method chaining',
+  author = 'Peter Downs',
+  author_email = 'philchiladki@yahoo.com',
+  url = 'https://github.com/PilCAki/fcollections.git', # use the URL to the github repo
+  download_url = 'https://github.com/PilCAki/fcollections/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
