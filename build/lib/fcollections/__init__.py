dc3c16153e77adfc114257741af1827b4d53521d
Phillip Adkins <padkins@pca.local>
2016-06-18 14:27:11 -0700

    all fixed up

---
+++ build/lib/fcollections/__init__.py
@@ -0,0 +1,2 @@
+from .fcollections import flist, fdict, fgenerator, frange, fxrange
+__all__ = ['flist', 'fdict', 'fgenerator', 'frange', 'fxrange']
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
   name = 'fcollections',
-  packages = ['fcollections'], # this must be the same as the name above
+  packages = ['fcollections'], 
   version = '0.1',
   description = 'collections with method chaining',
-  author = 'Peter Downs',
+  author = 'Phillip Adkins',
   author_email = 'philchiladki@yahoo.com',
   url = 'https://github.com/PilCAki/fcollections.git', # use the URL to the github repo
   download_url = 'https://github.com/PilCAki/fcollections/tarball/0.1', # I'll explain this in a second
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  keywords = ['chaining', 'functional', 'cytoolz', 'itertoolz'], # arbitrary keywords
   classifiers = [],
 )
